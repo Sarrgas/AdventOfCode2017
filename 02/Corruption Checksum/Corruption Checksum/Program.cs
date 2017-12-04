15f43f78089a26125977040ddc2dcea54167fa83
Andreas Smidelöv <sargas1@hotmail.com>
2017-12-04 18:18:54 +0100

    Initial Commit Day 2

---
+++ 02/Corruption Checksum/Corruption Checksum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corruption_Checksum
+{
+    class Program
+    {
+        public void Start()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+        }
+    }
+}

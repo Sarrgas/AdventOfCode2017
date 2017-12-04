d2d0f3793688a8898f0fec031fb69b70fb432c89
Andreas Smidelöv <sargas1@hotmail.com>
2017-12-04 18:53:54 +0100

    Solved it!

---
+++ 02/Corruption Checksum/Corruption Checksum/Row.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corruption_Checksum
+{
+    class Row
+    {
+        public int[] Data { get; set; }
+        public int Checksum { get; set; }
+
+        public Row(params int[] _data)
+        {
+            Data = _data;
+            Checksum = CalculateChecksum(_data);
+        }
+
+        private int CalculateChecksum(int[] row)
+        {
+            int smallest = Int32.MaxValue;
+            int largest = 0;
+
+            foreach (int number in row)
+            {
+                if (number < smallest)
+                    smallest = number;
+                if (number > largest)
+                    largest = number;
+            }
+
+            return largest - smallest;
+        }
+    }
+}

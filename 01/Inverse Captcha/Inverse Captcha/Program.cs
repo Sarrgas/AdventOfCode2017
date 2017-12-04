81e52c7f564ae9fec5d4e2d73b67e748f2a2a5d5
Andreas Smidelöv <sargas1@hotmail.com>
2017-12-04 17:43:23 +0100

    Initial commit
    Visual Studio Project setup

---
+++ 01/Inverse Captcha/Inverse Captcha/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inverse_Captcha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

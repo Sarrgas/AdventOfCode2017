c06c320855c3b69cb39b16469418d4f8d7997b7d
Andreas Smidelöv <sargas1@hotmail.com>
2017-12-05 20:25:39 +0100

    Initial commit

---
+++ 04/High-Entropy Passphrases/High-Entropy Passphrases/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace High_Entropy_Passphrases
+{
+    class Program
+    {
+        public void Start()
+        {
+
+        }
+        static void Main()
+        {
+            Program program = new Program();
+            program.Start();
+        }
+    }
+}
